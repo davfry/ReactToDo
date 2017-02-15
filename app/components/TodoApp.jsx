fad207365251ef9fa007d5bf5c010587db269789
davfry <davecfryer@gmail.com>
2017-02-15 12:42:55 -0700

    add todo app component

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,13 @@
+import React, { PropTypes } from 'react'
+
+const TodoApp = React.createClass({
+  render () {
+    return (
+      <div>
+        TodoApp.jsx
+      </div>
+    )
+  }
+})
+
+export default TodoApp
